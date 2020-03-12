90c16974c07c7e37f8c27b149260748eef238e3c
nkoshkarov <nkoshkarov@phoenixit.ru>
2020-03-12 03:57:43 +0300

    prepared to heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser         = require('body-parser');
 const port               = process.env.PORT || 5000;
 const app                = express();
 
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
